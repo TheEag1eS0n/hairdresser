3cb61de65a0380dce9bfcf760ded31e9b91c1692
TheEag1eS0n <egorgalembo@gmail.com>
2021-02-22 19:19:50 +0500

    Trying to add sliding panel feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:hairdresser/paint_page/paintpage.dart';
 
-void  main() => runApp(MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: PaintPage(),
+      home: Scaffold(
+        body: PaintPage(),
+      ),
     );
   }
 }
--- lib/paint_page/paintpage.dart
+++ lib/paint_page/paintpage.dart
@@ -1,7 +1,6 @@
 import 'dart:html';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:hairdresser/paint_page/canvasPainter.dart';
 import 'package:hairdresser/paint_page/tool.dart';
 import 'package:hairdresser/paint_page/tools/brush.dart';
@@ -30,7 +29,8 @@ class _PaintPageState extends State<PaintPage> {
   List<Tool> _shapeUndoCache = [];
 
   List<CanvasText> _textList = [];
-  int step = 0;
+
+  double _colorPanelHeight = 0;
 
   List<bool> get selected {
     var buttons = List.generate(4, (_) => false);
@@ -48,145 +48,163 @@ class _PaintPageState extends State<PaintPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          GestureDetector(
-            onPanStart: (event) {
-              setState(() {
-                _shapeUndoCache = [];
-                switch (_currentTool) {
-                  case DrawingTool.Brush:
-                    updateType = UpdateType.AddPoint;
-                    _shapeList.add(Brush(
+    return Stack(
+      children: [
+        GestureDetector(
+          onPanStart: (event) {
+            setState(() {
+              _shapeUndoCache = [];
+              switch (_currentTool) {
+                case DrawingTool.Brush:
+                  updateType = UpdateType.AddPoint;
+                  _shapeList.add(Brush(
+                      start: event.localPosition,
+                      paint: Paint()
+                        ..color = currentColor
+                        ..style = PaintingStyle.stroke
+                        ..strokeWidth = 5
+                        ..strokeCap = StrokeCap.round));
+                  break;
+                case DrawingTool.Curve:
+                  if (_shapeList.isEmpty ||
+                      (_shapeList.last.runtimeType != CurveLine ||
+                          !_shapeList.last.hitZone(event.localPosition))) {
+                    updateType = UpdateType.SetEndPoint;
+                    _shapeList.add(CurveLine(
                         start: event.localPosition,
                         paint: Paint()
                           ..color = currentColor
                           ..style = PaintingStyle.stroke
                           ..strokeWidth = 5
                           ..strokeCap = StrokeCap.round));
-                    break;
-                  case DrawingTool.Curve:
-                    if (_shapeList.isEmpty ||
-                        (_shapeList.last.runtimeType != CurveLine ||
-                            !_shapeList.last.hitZone(event.localPosition))) {
-                      updateType = UpdateType.SetEndPoint;
-                      _shapeList.add(CurveLine(
-                          start: event.localPosition,
-                          paint: Paint()
-                            ..color = currentColor
-                            ..style = PaintingStyle.stroke
-                            ..strokeWidth = 5
-                            ..strokeCap = StrokeCap.round));
-                    } else {
-                      updateType = UpdateType.SetCenterPoint;
-                    }
+                  } else {
+                    updateType = UpdateType.SetCenterPoint;
+                  }
 
-                    break;
-                  case DrawingTool.Eraser:
-                    updateType = UpdateType.AddPoint;
-                    _shapeList.add(Erase(start: event.localPosition));
-                    break;
-                  case DrawingTool.Text:
-                    if (_shapeList.isEmpty || (_shapeList.last.runtimeType != CanvasText ||
-                        !_shapeList.last.hitZone(event.localPosition)))
-                      _showDialog(event.localPosition);
-                    else
-                      _shapeList.last.start = event.localPosition;
-                    break;
-                }
-              });
-            },
-            onPanUpdate: (event) {
-              setState(() {
-                _shapeList.last.update(event.localPosition, updateType);
-              });
-            },
-            onPanEnd: (event) {},
-            child: CustomPaint(
-              foregroundPainter: ShapesCanvas(shapes: _shapeList),
-              child: Center(
-                child: Image.asset('images/bg-head.png'),
-              ),
+                  break;
+                case DrawingTool.Eraser:
+                  updateType = UpdateType.AddPoint;
+                  _shapeList.add(Erase(start: event.localPosition));
+                  break;
+                case DrawingTool.Text:
+                  if (_shapeList.isEmpty ||
+                      (_shapeList.last.runtimeType != CanvasText ||
+                          !_shapeList.last.hitZone(event.localPosition)))
+                    _showDialog(event.localPosition);
+                  else
+                    _shapeList.last.start = event.localPosition;
+                  break;
+              }
+            });
+          },
+          onPanUpdate: (event) {
+            setState(() {
+              _shapeList.last.update(event.localPosition, updateType);
+            });
+          },
+          onPanEnd: (event) {},
+          child: CustomPaint(
+            foregroundPainter: ShapesCanvas(shapes: _shapeList),
+            child: Center(
+              child: Image.asset('images/bg-head.png'),
             ),
           ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
+        ),
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: Container(
+              padding: EdgeInsets.all(0),
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ToggleButtons(
+                    children: <Widget>[
+                      Icon(
+                        Icons.brush,
+                        size: 20,
+                      ),
+                      Icon(
+                        Icons.call,
+                        size: 20,
+                      ),
+                      Icon(
+                        Icons.cake,
+                        size: 20,
+                      ),
+                      Icon(
+                        Icons.text_fields,
+                        size: 20,
+                      ),
+                    ],
+                    isSelected: selected,
+                    onPressed: (int index) {
+                      setState(() {
+                        _currentTool = DrawingTool.values[index];
+                        _colorPanelHeight = 100;
+                      });
+                      // Scaffold.of(context).showBottomSheet((context) => BottomSheet());
+                    },
+                    constraints: BoxConstraints(minHeight: 50, minWidth: 50),
+                    renderBorder: false,
+                    splashColor: currentColor.withOpacity(0.25),
+                    highlightColor: currentColor.withOpacity(0.5),
+                    selectedColor: currentColor,
+                    fillColor: currentColor.withOpacity(0.1),
+                  ),
+                  ButtonBar(
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.undo),
+                        splashRadius: 15,
+                        disabledColor: Colors.black.withOpacity(.5),
+                        padding: new EdgeInsets.all(0),
+                        constraints:
+                            BoxConstraints(minHeight: 30, minWidth: 30),
+                        onPressed: _shapeList.isEmpty
+                            ? null
+                            : () {
+                                setState(() {
+                                  _shapeUndoCache.add(_shapeList.removeLast());
+                                });
+                              },
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.redo),
+                        splashRadius: 20,
+                        disabledColor: Colors.black.withOpacity(.5),
+                        padding: new EdgeInsets.all(0),
+                        constraints:
+                            BoxConstraints(minHeight: 40, minWidth: 40),
+                        onPressed:
+                            _shapeUndoCache.isEmpty ? null : addRedoCache,
+                      ),
+                    ],
+                  )
+                ],
+              )),
+        ),
+        Positioned(
+          bottom: 50,
+          left: 0,
+          right: 0,
+          child: AnimatedContainer(
+            duration: Duration(milliseconds: 500),
+            curve: Curves.easeOut,
+            height: _colorPanelHeight,
+            color: Colors.grey[200],
             child: Container(
-                padding: EdgeInsets.all(0),
-                decoration: BoxDecoration(
-                  color: Colors.grey[200],
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    ToggleButtons(
-                      children: <Widget>[
-                        Icon(
-                          Icons.brush,
-                          size: 20,
-                        ),
-                        Icon(
-                          Icons.call,
-                          size: 20,
-                        ),
-                        Icon(
-                          Icons.cake,
-                          size: 20,
-                        ),
-                        Icon(
-                          Icons.text_fields,
-                          size: 20,
-                        ),
-                      ],
-                      isSelected: selected,
-                      onPressed: (int index) {
-                        setState(() {
-                          _currentTool = DrawingTool.values[index];
-                        });
-                      },
-                      constraints: BoxConstraints(minHeight: 50, minWidth: 50),
-                      renderBorder: false,
-                      splashColor: currentColor.withOpacity(0.25),
-                      highlightColor: currentColor.withOpacity(0.5),
-                      selectedColor: currentColor,
-                      fillColor: currentColor.withOpacity(0.1),
-                    ),
-                    ButtonBar(
-                      children: [
-                        IconButton(
-                            icon: Icon(Icons.undo),
-                            splashRadius: 15,
-                            disabledColor: Colors.black.withOpacity(.5),
-                            padding: new EdgeInsets.all(0),
-                            constraints:
-                                BoxConstraints(minHeight: 30, minWidth: 30),
-                          onPressed:
-                          _shapeList.isEmpty ? null : () {
-                              setState(() {
-                                _shapeUndoCache.add(_shapeList.removeLast());
-                              });
-                          },
-                        ),
-                        IconButton(
-                          icon: Icon(Icons.redo),
-                          splashRadius: 20,
-                          disabledColor: Colors.black.withOpacity(.5),
-                          padding: new EdgeInsets.all(0),
-                          constraints:
-                              BoxConstraints(minHeight: 40, minWidth: 40),
-                          onPressed:
-                              _shapeUndoCache.isEmpty ? null : addRedoCache,
-                        ),
-                      ],
-                    )
-                  ],
-                )),
+                height: 20,
+                width: 20,
+                color: Colors.red,
+            ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 
@@ -225,6 +243,19 @@ class _PaintPageState extends State<PaintPage> {
           ),
         ],
       ),
+    );
+  }
+}
+
+class BottomSheet extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Container(
+        height: 200,
+        color: Colors.amber,
+      ),
+      onTap: () => Navigator.pop(context),
     );
   }
 }
